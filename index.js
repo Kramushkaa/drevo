6c5f74c02af316f55c27d48825a37d7faa92e696
Kramushkaa <71790093+Kramushkaa@users.noreply.github.com>
2022-05-03 17:47:34 +0300

    trying to js it

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function makePerson (list,id) {
-    list.find(x => x.Number === id)
+    personData = list.find(x => x.Number === id)
     let person = `<div> <img src='Images/tree.png' alt = 'drevo'> 
-        <p class = 'fio'> ${" " + x.Birth_family_name || "" + ") "}${x.Family_name} ${x.First_name} ${x.Fathers_name}</p>`;
-    if (x.children.length > 0) {
+        <p class = 'fio'> ${" " + personData.Birth_family_name || "" + ") "}${personData.Family_name} 
+        ${personData.First_name} ${personData.Fathers_name}</p>`;
+    if (personData.children.length > 0) {
         person += `<ul>`
-        for (let i = 0; i < x.children.length; i++) {
-            person += `<li> + makePerson (list,x.children[i]) + </li>`
+        for (let i = 0; i < personData.children.length; i++) {
+            person += `<li> + ${makePerson (list,personData.children[i])} + </li>`
         }
         person += `/<ul>`
     }
