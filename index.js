b87251e0d6728fc79541f22f99f4011a9336e230
Kramushkaa <71790093+Kramushkaa@users.noreply.github.com>
2022-05-03 17:34:35 +0300

    trying to js it

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-async function getdata () {
+function makePerson (list,id) {
+    list.find(x => x.Number === id)
+    let person = `<div> <img src='Images/tree.png' alt = 'drevo'> 
+        <p class = 'fio'> ${" " + x.Birth_family_name || "" + ") "}${x.Family_name} ${x.First_name} ${x.Fathers_name}</p>`;
+    if (x.children.length > 0) {
+        person += `<ul>`
+        for (let i = 0; i < x.children.length; i++) {
+            person += `<li> + makePerson (list,x.children[i]) + </li>`
+        }
+        person += `/<ul>`
+    }
+
+    person += `</div>`
+    return person
+}
+
+async function getData () {
     let response = await fetch('datafile.json');
 
     let parsed = await response.json(); 
 
-    console.log(parsed)
+    console.log(makePerson(parsed,1))
 }
 
-getdata()
+getData();
