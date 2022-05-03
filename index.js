e7577049c0d04da39bb1db0eac33bed335590748
Kramushkaa <71790093+Kramushkaa@users.noreply.github.com>
2022-05-04 00:49:03 +0300

    minor us improvements

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ function makePerson (list,id) {
     personData = list.find(x => x.Number === id)
     let fio = personData.Family_name + " " + (personData.Birth_family_name ? "(" + personData.Birth_family_name + ") " : "") 
             + personData.First_name + " " + personData.Fathers_name;
-    let person = `<div class="person ${personData.Sex === "m" ? "male" : "female"}"> 
+    let person = `<div class = "card-box"> <div class="person ${personData.Sex === "m" ? "male" : "female"}"> 
                     <div class="card-front"> 
                         <img src='Images/tree.png' alt = 'drevo'> 
                         <p class = 'fio'>${fio}</p></div>
                     <div class="card-back"> 
                         <p class=fio>${fio}</p>
                         ${personData.Death_date ? `<p class="dates-of-life"> Даты жизни: ${personData.Birth_date}-${personData.Death_date}` : `<p class="birthday"> Дата рождения: ${personData.Birth_date}`} </p>
-                        <button onclick="$dc.loadPerson(${personData.Number});">Больше</button></div></div>`
+                        <button onclick="$dc.loadPerson(${personData.Number});">Больше</button></div></div></div>`
 
     let children = personData.Children;
 
