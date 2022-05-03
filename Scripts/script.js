0f5d890c56ed4fc7c5f914af3cf18d008c9d5ffd
Kramushkaa <71790093+Kramushkaa@users.noreply.github.com>
2022-05-03 23:30:08 +0300

    получилось доработать карточку информации и дргуие js-штуки

--- Scripts/script.js
+++ Scripts/script.js
@@ -11,8 +11,33 @@ var insertHtml = function (selector, html) {
   };
 
 
-dc.loadPerson = function (person_number) {
-    html="<div id=person-info> <h2> Kramushka </h2> <button id='closer' onclick='$dc.closePersonInfo(&quot;Kramushka&quot;)';>Закрыть</button> </div>"
+dc.loadPerson = async function (id) {
+    let parsed=await getData()
+    let personData = await parsed.find(x => x.Number === id)
+    let fio = personData.Family_name + " " + (personData.Birth_family_name ? "(" + personData.Birth_family_name + ") " : "") 
+            + personData.First_name + " " + personData.Fathers_name;
+    let html=`<div id=person-info> 
+                <button id='closer' onclick='$dc.closePersonInfo(${personData.Number})';>Закрыть</button>
+                <h2> ${fio} </h2> 
+                <p> Дата рождения: ${personData.Birth_date} </p>
+                <p> Родной город: ${personData.Birth_city} </p>`
+    if (personData.Death_date) {
+        html += `<p> Дата смерти: ${personData.Death_date}
+        <p> Похоронен: ${personData.Current_city} </p>`
+    } else {
+        html += `<p> Сейчас живёт: ${personData.Current_city} </p>`
+    }
+    if (personData.Children.length>0) {
+        html += `<div id="children"> <h3> Дети </h3>`
+        for (child of personData.Children) {
+            let childData = await parsed.find(x => x.Number === child)
+            let fio = childData.Family_name + " " + (childData.Birth_family_name ? "(" + childData.Birth_family_name + ") " : "") 
+            + childData.First_name + " " + childData.Fathers_name;
+            html += `<div class="child"> ${fio} <button onclick="$dc.loadPerson(${childData.Number});">Перейти</button></div>`
+        }
+        html += `</div>`
+    }
+    html += `</div>`
     insertHtml('#person-info-holder',html)
 };
 
--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 function makePerson (list,id) {
     personData = list.find(x => x.Number === id)
-    let person = `<div> <img src='Images/tree.png' alt = 'drevo'> 
-        <p class = 'fio'> ${(personData.Birth_family_name ? "(" + personData.Birth_family_name + ") " : "")}${personData.Family_name} 
-        ${personData.First_name} ${personData.Fathers_name}</p></div>`;
-    let children = personData.Children
-    console.log(children.length)
+    let fio = personData.Family_name + " " + (personData.Birth_family_name ? "(" + personData.Birth_family_name + ") " : "") 
+            + personData.First_name + " " + personData.Fathers_name;
+    let person = `<div class="person ${personData.Sex === "m" ? "male" : "female"}"> 
+                    <div class="card-front"> 
+                        <img src='Images/tree.png' alt = 'drevo'> 
+                        <p class = 'fio'>${fio}</p></div>
+                    <div class="card-back"> 
+                        <p class=fio>${fio}</p>
+                        ${personData.Death_date ? `<p class="dates-of-life"> Даты жизни: ${personData.Birth_date}-${personData.Death_date}` : `<p class="birthday"> Дата рождения: ${personData.Birth_date}`} </p>
+                        <button onclick="$dc.loadPerson(${personData.Number});">Больше</button></div></div>`
+
+    let children = personData.Children;
+
     if (children.length > 0) {
         person += `<ul>`
         for (let i = 0; i < children.length; i++) {
@@ -21,9 +29,16 @@ async function getData () {
 
     let parsed = await response.json(); 
 
+    return parsed
+
+}
+
+async function makeatree() {
+    let parsed = await getData()
+
     htmlTree = makePerson(parsed,1);
 
     document.querySelector(".outer li").innerHTML = htmlTree;
 }
 
-getData();
+makeatree();
